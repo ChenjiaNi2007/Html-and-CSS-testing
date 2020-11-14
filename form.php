dd6d0e04cc7862a9b708223d6f4977a8ab9b38a8
Jeffernickel <chenjiani2007@gmail.com>
2020-11-14 20:18:07 +0000

    Created php to include

--- form.php
+++ form.php
@@ -3,14 +3,14 @@
 <html>
 
   <head>
-    <title> Contacts </title>
+    <title> Submission End Page </title>
     <link rel="stylesheet" type="text/css" href="style.css" />
   </head>
 
   <body>
     <div id="container">
       <div id="header">
-        <h1> Contact </h1>
+        <h1> Form Submission End </h1>
       </div> 
       <div id="content">
         <div id="nav">
@@ -18,15 +18,18 @@
           <ul>
             <li> <a href="index.html">Home</a> </li>
             <li> <a href="about.html"> About</a> </li>
-            <li> <a class="selected" href="contact.html"> Contact</a> </li>
-            <li> <a href="form.html"> Form </a> </li>
+            <li> <a  href="contact.html"> Contact</a> </li>
+            <li> <a class="selected" href="form.html"> Form </a> </li>
         </div>
 
         <div id="main">
-          <h2> Contact Page </h2>
-          <p> You can contact me at: <strong> stonks4life2007@gmail.com </strong> </p>
-          <p> Phone number is: <strong> 858-412-6969 </strong> </p>
-          <p> <i> (please don't contact the number above) </i> </p>
+          <h2> Form Submission End </h2>
+          <?php
+            $name = $_POST["name"];
+            echo $name;
+          ?>
+          <p> Please go to another page, I hate it here. </p>
+          <p> I don't need another person reminding me of my suffering </p>
         </div>
 
       </div>
